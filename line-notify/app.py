d7f847d1cda36666b719eb5b6628d9fc5938fd45
maloyang <33815331+maloyang@users.noreply.github.com>
2018-06-17 07:21:35 +0800

    Add files via upload

---
+++ line-notify/app.py
@@ -0,0 +1,88 @@
+# coding=UTF-8
+from __future__ import unicode_literals
+
+from flask import Flask, render_template, request
+from flask import json, jsonify
+
+from datetime import datetime
+import time
+import os
+import pprint
+
+import random
+import json
+import requests
+
+#------
+#   line nofity功能 web-api
+#------
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def homepage():
+    return "OK"
+
+
+###############
+# Line API
+###############
+# group notify token 'lEwq4fFJtphsPwkvqHwdb92g6nrqXS6bYn56kL068sP'
+@app.route('/line', methods=['GET', 'POST'])
+def line():
+    url = "https://notify-api.line.me/api/notify"
+
+    token = request.args.get('token')
+    message = request.args.get('message')
+
+    headers = {"Authorization" : "Bearer "+ token}
+
+    payload = {"message" :  message}
+
+    try:
+        r = requests.post(url ,headers = headers ,params=payload)
+    except Exception as ex:
+        print('line_notify() error: ', str(ex))
+        return 'ERROR'
+    
+    return 'OK'
+
+@app.route('/line2', methods=['GET', 'POST'])
+def line2():
+    url = "https://notify-api.line.me/api/notify"
+
+    print('--.__dict__--')
+    pprint.pprint(request.__dict__)
+    print('--.data--')
+    pprint.pprint(request.data)
+    print('--.form--')
+    pprint.pprint(request.form)
+    print('--.args--')
+    pprint.pprint(request.args)
+    print('----')
+
+    '''
+    token = request.form['token']
+    message = request.form['message']
+    print(token, message)
+    '''
+    data = json.loads(request.data)
+    token = data['token']
+    message = data['message']
+
+    headers = {"Authorization" : "Bearer "+ token}
+
+    payload = {"message" :  message}
+
+    try:
+        r = requests.post(url ,headers = headers ,params=payload)
+    except Exception as ex:
+        print('line_notify() error: ', str(ex))
+        return 'ERROR: '+str(ex)
+    
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
